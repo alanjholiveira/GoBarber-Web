b8d2dbb0f324594716c9400c033922d0410aa39f
Alan Oliveira <alan@thinkpad-x250.localdomain>
2020-01-11 20:28:36 -0300

    Configurado autenticação, persistindo autenticação no localStorage usando redux-persist, implementado loading no botão de login

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
+import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
+
 import createStore from './createStore';
+import persistReducers from './persistReducers';
 
 import rootReduce from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
@@ -13,8 +16,9 @@ const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const middleware = [sagaMiddleware];
 
-const store = createStore(rootReduce, middleware);
+const store = createStore(persistReducers(rootReduce), middleware);
+const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export { store, persistor };
