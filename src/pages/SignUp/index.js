ab0c96255aa1a7fd1c72fb04516d9631fa4dfef9
Alan Oliveira <alan@thinkpad-x250.localdomain>
2020-01-11 18:11:11 -0300

    GoBarber Web Aplicação em ReactJS Refatoração

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatoria'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatorio'),
+  password: Yup.string()
+    .min(6, 'No minimo 6 catactes')
+    .required('A senha é obrigatoria'),
+});
+
+export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
+}
